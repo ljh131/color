d10f533caa31b12fc76be32d0e24f61c6b5d22a5
ljh131 <ljh131@gmail.com>
2015-04-22 18:00:12 +0900

    added usage function, modified to ignore stack trace for ctrl+c exception

--- color.py
+++ color.py
@@ -17,76 +17,95 @@ WHITE	 = "\033[1;37m"
 COLORS = [RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE]
 RESET	 = "\033[0m"
 
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "i", ["help"])
-except Exception as err:
-	print(str(err))
-	sys.exit(2)
+def usage():
+	print('usage: %s <options> <patterns>', sys.argv[0])
+	print('')
+	print('options:')
+	print('    -i: ignore case')
+	print('patterns:')
+	print('    <string>:<color>')
+	print('colors:')
+	print('    %sr[ed]%s, %sg[reen]%s, %sy[ellow]%s, %sb[lue]%s, %sm[agenta]%s, %sc[yan]%s, %sw[hite]%s' % ( 
+			RED, RESET, 
+			GREEN, RESET,
+			YELLOW, RESET,
+			BLUE, RESET,
+			MAGENTA, RESET,
+			CYAN, RESET,
+			WHITE, RESET,
+	))
+	print('')
+	print('eg)')
+	print('    echo "hello color" | %s "hello:r" "o:b" "c:y"' % sys.argv[0])
 
-if len(args) == 0:
-	print('need pattern')
-	for cr in COLORS:
-		text = 'these are sample colors: RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE'
-		print('%s%s%s' % (cr, text, RESET))
-	sys.exit(2)
-
-ignorecase = False
-
-for o, a in opts:
-	if o in ("-h", "--help"):
-		print('no help')
+def main():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "i", ["help"])
+	except Exception as err:
+		usage()
 		sys.exit(2)
-	elif o in ('-i'):
-		ignorecase = True
 
-patterns = []	# array of dict(pattern=pat, color=cr)
-cridx = 0
-for a in args:
-	ar = a.split(':')
-	if len(ar) == 1:
-		# pattern only
-		pat = ar[0]
-		cr = COLORS[cridx % len(COLORS)]
-		cridx += 1
-	else:
-		# pattern:color (could be abbr.)
-		pat = ar[0]
-		color = ar[1]
-		if color.startswith('r'):
-			cr = RED
-		elif color.startswith('g'):
-			cr = GREEN
-		elif color.startswith('y'):
-			cr = YELLOW 
-		elif color.startswith('b'):
-			cr = BLUE
-		elif color.startswith('m'):
-			cr = MAGENTA
-		elif color.startswith('c'):
-			cr = CYAN
-		elif color.startswith('w'):
-			cr = WHITE
+	if len(args) == 0:
+		usage()
+		sys.exit(2)
+
+	ignorecase = False
+
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print('no help')
+			sys.exit(2)
+		elif o in ('-i'):
+			ignorecase = True
+
+	patterns = []	# array of dict(pattern=pat, color=cr)
+	cridx = 0
+	for a in args:
+		ar = a.split(':')
+		if len(ar) == 1:
+			# pattern only
+			pat = ar[0]
+			cr = COLORS[cridx % len(COLORS)]
+			cridx += 1
 		else:
-			cr = WHITE
+			# pattern:color (could be abbr.)
+			pat = ar[0]
+			color = ar[1]
+			if color.startswith('r'):
+				cr = RED
+			elif color.startswith('g'):
+				cr = GREEN
+			elif color.startswith('y'):
+				cr = YELLOW 
+			elif color.startswith('b'):
+				cr = BLUE
+			elif color.startswith('m'):
+				cr = MAGENTA
+			elif color.startswith('c'):
+				cr = CYAN
+			elif color.startswith('w'):
+				cr = WHITE
+			else:
+				cr = WHITE
 
-	patterns.append(dict(pat='(%s)' % pat, sub='%s\g<1>%s' % (cr, RESET)))
+		patterns.append(dict(pat='(%s)' % pat, sub='%s\g<1>%s' % (cr, RESET)))
 
-if ignorecase:
-	reflags = re.IGNORECASE
-else:
-	reflags = 0
+	if ignorecase:
+		reflags = re.IGNORECASE
+	else:
+		reflags = 0
 
-#test='hello world\nabcd\n1234'
-#for line in test.split('\n'):
+	try:
+		while True:
+			line = sys.stdin.readline()
+			if not line: break
 
-#for line in fileinput.input():
-#for line in sys.stdin:
-while True:
-	line = sys.stdin.readline()
-	if not line: break
+			for d in patterns:
+				line = re.sub(d['pat'], d['sub'], line, flags=reflags)
+			print(line, end='')
+			sys.stdout.flush()
+	except KeyboardInterrupt:
+		pass
 
-	for d in patterns:
-		line = re.sub(d['pat'], d['sub'], line, flags=reflags)
-	print(line, end='')
-	sys.stdout.flush()
+main()
 
